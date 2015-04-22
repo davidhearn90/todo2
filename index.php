7027563ebf91da08bf439256e9f1e6c551d870b7
David Hearn <davidhearn90@gmail.com>
2015-04-22 08:54:25 -0700

    finished milestone 6

--- index.php
+++ index.php
@@ -6,8 +6,17 @@
 </head>
 <body>
 	<div class="wrap">
+		<div class="task-list">
+		<ul>
+			<?php require("includes/connect.php"); ?>
+		</ul>
+			
+		</div>
 	
 	</div>
-
+	<form class="add-new-task" autocomplete="off">
+		<input type="text1" name="new-task" placeholder="Add new item..." />
+	</form>
+</div>
 </body>
 </html>
