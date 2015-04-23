4ef22101e53c3cdbdb86efde51c5ef4a122564a8
David Hearn <davidhearn90@gmail.com>
2015-04-23 08:55:18 -0700

    finished milestone 19

--- includes/add-task.php
+++ includes/add-task.php
@@ -1,15 +1,15 @@
-<?php 
-$task = strip_tags($_POST['task']);
-$date = date('Y-m-d');
-$time = date('H:i:s');
+<?php
+$task = strip_tags($_POST["task"]);
+$date = date("Y-m-d");
+$time = date("H:i:s");
 include("connect.php");
-$mysqli = new mysqli("localhost", "root", "root", "tasks");
+$mysqli = new mysqli("localhost", "root", "root", "todo");
 $mysqli ->query("INSERT INTO tasks VALUES ('','$task', '$date', '$time')");
 $query = "SELECT * FROM tasks WHERE task='$task' and date='$date' and time='$time'";
-if ($result = $mysqli-.query(query)) {
-	while ($row = $result->fetch_assoc()) {
+if($result = $mysqli->query($query)){
+	while ($row = $result->fetch_assoc()){
 		$task_id = $row['id'];
-		$task_name= $row['task'];
+		$task_name = $row['task'];
 	}
 }
 $mysqli->close();
