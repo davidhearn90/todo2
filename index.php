b07f092956c8b953a0e9cb6f67d2bc4a41b8dab4
David Hearn <davidhearn90@gmail.com>
2015-04-23 08:41:56 -0700

    finished milestone 14

--- index.php
+++ index.php
@@ -1,37 +1,45 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Simple To-Do List</title>
-	<link rel="stylesheet" type="text/css" href="css/main.css">
+	<title>Brian's Awesome To-Do List</title>
+	<link rel="stylesheet" type='text/css' href"CSS/main.css">
 </head>
 <body>
-	<div class="wrap">
-		<div class="task-list">
-		<ul>
-			<?php require("includes/connect.php"); ?>
-
-		</ul>
-			
-		</div>
-	
-	</div>
-	<form class="add-new-task" autocomplete="off">
-		<input type="text" name="new-task" placeholder="Add new item..."/>
-	</form>
-	<script>
-	add_task(); //calling the add task function
-	function add_task(){
-		$(".add-new-task").submit(function(){
-			var new_task = $(".add-new-task input[name=new-task]").val();
-  	if(new_task != ""){
-		$.post("includes/add-task.php", {task: new_task}, function(data){
-   		$("add-new-task input[name=new-task]").val();
-		$(data).appendTo("task-list ul").hide().fadeIn();
-	});
-    return false;
-	});
-}
-</script>
+<div class="wrap">
+<div class = "task-list">
+<ul>
+<?php require("includes/connect.php"); ?>
+</ul>
+</div>
+<form class="add-new-task" autocomplete="off">
+<input type="text" name="new-task" placeholder="Add new item..."/>
+</form>
 </div>
 </body>
+<script src="http://code.jquery.com/jquery-latest.min.js"></script>
+<script>
+	add_task(); //calling the add task function
+function add_task(){
+	$(".add-new-task").submit(function(){
+var new_task = $(".add-new-task input[name=new-task]").val();
+if(new_task != ""){
+	$.post("includes/add-task.php", {task: new_task}, function(data){
+$(("add-new-task input[name=new-task]").val();
+	$(data).appendTo("task-list ul").hide().fadeIn();
+	});
+}
+return false;
+	});
+}
+$('.delete-button').click(function(){
+	var current_element = $(this);
+	var task_id = $(this).attr('id');
+	$.post('includes/delete-task.php', {id: task_id}, function(){
+current_element.parent().fadeOut("fast", function(){
+	$(this).remove();
+});
+});
+});
+</script>
+
 </html>
