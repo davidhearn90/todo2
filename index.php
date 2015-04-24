d226337b529a4f6a253f67fc930fc567e11c74ee
David Hearn <davidhearn90@gmail.com>
2015-04-24 15:48:18 -0700

    login

--- index.php
+++ index.php
@@ -1,10 +1,8 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>  To-Do List</title>
-	<link rel="stylesheet" type='text/css' href="CSS/main.css">
-	<link rel="stylesheet" type="text/css" href="CSS/normalize.css">
-	<link rel="stylesheet" type="text/css" href="CSS/reset.css">
+	<title> To-Do List</title>
+	<link rel="stylesheet" type='text/css' href="css/main.css">
 </head>
 <body>
 <div class="wrap">
@@ -16,12 +14,12 @@ $query = "SELECT * FROM tasks ORDER BY date ASC, time ASC";
 if ($result = $mysqli->query($query)) {
 	$numrows = $result->num_rows;
 if ($numrows>0) {
-		while($row = $result->fetch_assoc()){
+		while($row = $result->fetch_assoc()) {
 			$task_id = $row['id'];
-			$task_name = $row["task"];
+			$task_name = $row['task'];
 			echo '<li>
 			<span> '.$task_name.'</span>
-			<img id="'.$task_id.'"" class="delete button" width="10px" src="images/close.svg"/>
+			<img id="'.$task_id.'"" class="delete-button" width="10px" src="images/close.svg"/>
 			</li>';
 		}
 	}	
@@ -35,16 +33,16 @@ if ($numrows>0) {
 </form>
 </div>
 </body>
-<script src="http://code.jquery.com/jquery-latest.min.js"></script>
+<script src="https://code.jquery.com/jquery-latest.min.js"></script>
 <script>
 	add_task(); //calling the add task function
 function add_task(){
 	$(".add-new-task").submit(function(){
 var new_task = $(".add-new-task input[name=new-task]").val();
-if(new_task != ""){
+if(new_task !== ""){
 	$.post("includes/add-task.php", {task: new_task}, function(data){
-$(("add-new-task input[name=new-task]").val();
-	$(data).appendTo("task-list ul").hide().fadeIn();
+$("add-new-task input[name=new-task]").val();
+	$(data).appendTo(".task-list ul").hide().fadeIn();
 	});
 }
 return false;
---
+++ model/config.php
@@ -0,0 +1,17 @@
+<?php
+    require_once(__DIR__ . "/database.php");
+    session_start();
+    
+    $path = "/todo2/";
+    
+    $host = "localhost";
+    $username = "root";
+    $password = "root";
+    $database = "todo2";
+    
+    if(!isset($_SESSION["connection"])) {
+        $connection = new Database($host, $username, $password, $database);
+        $_SESSION["connection"] = $connection;
+    }
+    
+    
